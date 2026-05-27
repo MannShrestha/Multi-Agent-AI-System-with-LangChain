878bf24187ba913773e8df66903553eb36c73e62
MannShrestha <mannshrestha01@gmail.com>
2026-05-27 15:11:28 +0100

    Chaining pipeline for the agents.

--- main.py
+++ main.py
@@ -1,7 +1,10 @@
 from src.tools.tools import web_search,scrape_url
-
+from src.pipelines.pipeline import run_research_pipeline
 # output = web_search.invoke("Latest news on AI research")
 # print(output)
 
-results = scrape_url.invoke("https://arize.com/ai-research-papers/")
-print(results)
+# results = scrape_url.invoke("https://arize.com/ai-research-papers/")
+# print(results)
+
+topic = "The impact of AI on the job market in 2026"
+run_research_pipeline(topic)
