0e2aa34e463a8e7cb931b514a96df555c5fd80ce
MannShrestha <mannshrestha01@gmail.com>
2026-05-27 12:47:45 +0100

    folder structure created

---
+++ template.sh
@@ -0,0 +1,19 @@
+# Creating directory 
+mkdir -p src
+mkdir -p src/agents
+mkdir -p src/pipelines
+mkdir -p src/tools
+
+
+# Creating files
+touch src/__init__.py
+touch src/agents/__init__.py
+touch src/agents/agents.py  
+touch src/pipelines/__init__.py
+touch src/pipelines/pipeline.py
+touch src/tools/__init__.py
+touch src/tools/tools.py
+touch .env
+touch main.py
+touch app.py
+touch requirements.txt
