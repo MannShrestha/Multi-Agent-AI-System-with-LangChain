de0f39e92327a25e36d3232ec62f7bf5b9ef508b
MannShrestha <mannshrestha01@gmail.com>
2026-05-27 14:00:54 +0100

    using scrape_url to extract the text

--- main.py
+++ main.py
@@ -1,4 +1,7 @@
-from src.tools.tools import web_search
+from src.tools.tools import web_search,scrape_url
 
-output = web_search.invoke("Latest news on AI research")
-print(output)
+# output = web_search.invoke("Latest news on AI research")
+# print(output)
+
+results = scrape_url.invoke("https://arize.com/ai-research-papers/")
+print(results)
